b8c3a70d28c5946f0c1748917ab76b4388246609
Wizbeans <dixitmadhavi@yahoo.com>
2021-06-27 16:06:07 +0530

    final commit

---
+++ hello.js
@@ -0,0 +1,45 @@
+console.log(" Welcome to wizbeans's healthcare");
+alert("hello dear customer");
+ var var1 = prompt("enter your age");
+ if (0<var1<=18){
+     console.log("please go to kids speciality section");
+ }
+ else if (19<var1<=100){
+     console.log("please go to adult speciality section");
+ }
+
+ else{
+     console.log("please enter a valid age");
+ }
+
+/*
+ var i = 0;
+ var images = [];
+ var time = 3000;
+
+// image list
+images[1] = 'ambulance.jpg';
+images[2] = 'health.jpg';
+//images[3] = 'image3.jpg';
+
+ // change image
+
+ function changeImage(){
+     document.hasChildNodes.src = images[i];
+
+     if(i< images.length - 1){
+         i++;
+
+     } else {
+         i = 0;
+     }
+
+     setTimeout("changeImage()", time);
+
+
+
+
+ }
+
+
+ window.onload = changeImg;  */
